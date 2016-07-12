0e0164f2fe912912f9e2aff46f10f530350ecd9f
Dana Scheider <dana.scheider@gmail.com>
2016-07-12 11:14:14 -0700

    Complete application logic

--- personal_calculator.rb
+++ personal_calculator.rb
@@ -1,11 +1,13 @@
 require "sinatra"
 require "json"
 
+require_relative "./lib/age_calculator"
+
 post "/age" do 
   @request_body = JSON.parse(request.body.read, symbolize_names: true)
 
   {
     request: @request_body,
-    age:     20
+    age:     AgeCalculator.new(@request_body.fetch(:data).fetch(:birthdate)).get_age!
   }.to_json
 end
